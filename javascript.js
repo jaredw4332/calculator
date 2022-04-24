9526c8faf21c026e27bcd614367931facd33e6fa
Jared <jared.wilson4332@gmail.com>
2022-04-24 17:57:38 -0600

    Build basic math functions

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const add = function(a,b) {
+    return a + b
+};
+
+const subtract = function(a,b) {
+    return a - b
+};
+
+const multiply = function(a,b) {
+    return a * b
+}
+
+const divide = function(a,b) {
+    return a / b
+}
